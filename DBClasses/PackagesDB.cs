6354aef66c85606d7cb098ede603927c21ff80b0
Hayden Belanger <haydenbelanger@live.com>
2019-01-15 14:17:17 -0700

    .

--- DBClasses/PackagesDB.cs
+++ DBClasses/PackagesDB.cs
@@ -8,9 +8,14 @@ using System.Threading.Tasks;
 namespace DBClasses {
 	public static class PackagesDB {
 
-		public static Packages GetPackages() {
-			using(SqlConnection con = new SqlConnection()) {
+		public static Packages GetPackages(int supplierID) {
+			using(SqlConnection con = new SqlConnection(ConnectionString.Connection.Value())) {
+				con.Open();
+				using(SqlCommand cmd = new SqlCommand("SELECT * FROM Products PD FULL OUTER JOIN Products_Suppliers PS ON PS.ProductId = PD.ProductId WHERE PS.SupplierId = @sid;", con)) {
+					cmd.Parameters.AddWithValue("@sid", supplierID);
 
+					cmd.ExecuteReader();
+				}
 			}
 
 			return null;
