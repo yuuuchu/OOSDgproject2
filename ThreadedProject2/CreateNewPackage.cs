54a8fa9ec710cad9e1710ebf87e5a3dac4a8a66e
Hayden Belanger <haydenbelanger@live.com>
2019-01-23 12:41:21 -0700

    Comments added
    Explained code to Eugenia

--- ThreadedProject2/CreateNewPackage.cs
+++ ThreadedProject2/CreateNewPackage.cs
@@ -23,13 +23,12 @@ namespace ThreadedProject2
     public partial class CreateNewPackage : Form
     {
         //Create new empty BindingList and BindingSource
-		BindingList<KeyValuePair<string, int>> FilteredList = new BindingList<KeyValuePair<string, int>>();
-		BindingSource FilterSource = new BindingSource();
+		BindingList<KeyValuePair<string, int>> FilteredList = new BindingList<KeyValuePair<string, int>>();//list on the left
+		BindingSource FilterSource = new BindingSource(); //using a bindingsource, makes it so that when you add a value to the list, it will auto update the list view. No button is needed to refresh. 
 
-		BindingList<KeyValuePair<string, int>> SelectedList = new BindingList<KeyValuePair<string, int>>();
-		BindingSource SelectedSource = new BindingSource();
-
-		private static readonly KeyValuePair<string, int> DummyVal = new KeyValuePair<string, int>("ERR", -999);
+		BindingList<KeyValuePair<string, int>> SelectedList = new BindingList<KeyValuePair<string, int>>(); //list on the right
+		BindingSource SelectedSource = new BindingSource(); //using a bindingsource, makes it so that when you add a value to the list, it will auto update the list view. No button is needed to refresh. 
+		private static readonly KeyValuePair<string, int> DummyVal = new KeyValuePair<string, int>("ERR", -999); //readonly means that you cannot change this value
 
 		private static readonly Color ColorEnabled = Color.FromArgb(82, 117, 167);
 		private static readonly Color ColorDisabled = Color.Gray;
@@ -78,6 +77,12 @@ namespace ThreadedProject2
 					con.Open();
 
                     //sql Command
+					//PD is assigning a variable value to the table Products so that PD can be used throughout the query string.
+					//FULL OUTER JOIN means taking a FK on one table and a FK on another table that matches and getting values from the joined tables with the same FK.
+					//
+					//RESEARCH WHAT OUTER AND INNER JOIN MEANS
+					//WHERE means filtering a selection to only show matching values. @prod is needed because the string needs to proper 'escape' (convert) to only text values.
+					//This is so that it will not break the data in the table. 
                     string sqlCommand = "SELECT * FROM Products PD FULL OUTER JOIN Products_Suppliers PS ON " + 
                                         "PS.ProductId = PD.ProductId FULL OUTER JOIN Suppliers SP ON "+
                                         "SP.SupplierId = PS.SupplierId WHERE PS.ProductId = @prod;";
@@ -85,13 +90,15 @@ namespace ThreadedProject2
                     //using the connection and the string command,
                     using (SqlCommand cmd = new SqlCommand(sqlCommand, con)) {
 
-                        //Ask Hayden about this part
+                        //Take the selected value from the combo box and list/show all the values from the database into this list
+						//Get value and assign to parameter.
 						cmd.Parameters.AddWithValue("@prod", comboBoxProducts.SelectedValue);
 
                         //using the data reader, read the data while there is still data in table
 						using (SqlDataReader sdr = cmd.ExecuteReader()) {
 							while (sdr.Read()) {
 
+								//while there is still data, add it to the list view
 								FilteredList.Add(new KeyValuePair<string, int>(sdr.GetString(6), sdr.GetInt32(4)));
 
 							}
@@ -100,11 +107,15 @@ namespace ThreadedProject2
 				}
 
 				
+				//This method scans through the selectedList and the FilteredList 
+				//and compares the two lists. If the product exists on the right list, remove that product from the left list
 
 				foreach(KeyValuePair<string, int> kva in SelectedList) {
 
 					bool found = false;
 					KeyValuePair<string, int> kvX = DummyVal;
+					//string is the product name, int is the product ID
+					//for each key Value pair, if the product is on the right list, removed it from the left list. 
 					foreach(KeyValuePair<string, int> kvb in FilteredList) {
 						if(kva.Value == kvb.Value) {
 							found = true;
@@ -113,6 +124,7 @@ namespace ThreadedProject2
 						}
 					}
 
+					//this is the remove statement
 					if (found) {
 						FilteredList.Remove(kvX);
 					}
@@ -131,11 +143,15 @@ namespace ThreadedProject2
 			this.Close();
 		}
 
+		//this method is when you press the right button to add to the new package list
+		// Currently, you can only add five values to this empty list for new packages 
+
 		private void BtnAddSelected(object sender, EventArgs e) {
 			if(btnAdd.Enabled == true) {
-				if (lstSuppliers.SelectedValue != null) {
-					KeyValuePair<string, int> val = DummyVal;
-					foreach (KeyValuePair<string, int> v in FilteredList) {
+				if (lstSuppliers.SelectedValue != null) { //this checks to make sure that the left list has a value selected
+					KeyValuePair<string, int> val = DummyVal; //DummyVal is needed because the key value pair cannot be null. See above for variable
+
+					foreach (KeyValuePair<string, int> v in FilteredList) { //filtered list 
 						if (v.Value == (int)lstSuppliers.SelectedValue) {
 							val = v;
 							break;
