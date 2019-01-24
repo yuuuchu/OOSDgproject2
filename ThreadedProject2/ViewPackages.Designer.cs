23ea2d0d4080cc029730544ac0c0d72cc2407c76
yuuuchu <44778601+yuuuchu@users.noreply.github.com>
2019-01-24 13:45:11 -0700

    .

--- ThreadedProject2/ViewPackages.Designer.cs
+++ ThreadedProject2/ViewPackages.Designer.cs
@@ -169,7 +169,7 @@
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(253, 32);
             this.comboBox1.TabIndex = 16;
-            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+      
             //
             // btnEdit
             //
